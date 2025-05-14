9d0714a29e15a701c28f07eb30bdde772e1af5dd
Jack Kelly <jack@openclimatefix.org>
2025-05-14 20:05:26 +0100

    Loading data from GCS month-by-month appears to work! Running now!

--- marimo_notebooks/ARCO_ERA5_to_parquet.py
+++ marimo_notebooks/ARCO_ERA5_to_parquet.py
@@ -8,12 +8,12 @@ app = marimo.App(width="full")
 def _():
     import numpy as np
     import polars as pl
-    from datetime import datetime
+    from datetime import date, datetime, timedelta
     import xarray as xr
     import marimo as mo
     import pytest
     from clean_uk_pv import geospatial
-    return datetime, geospatial, mo, pl, xr
+    return date, datetime, geospatial, pl, timedelta, xr
 
 
 @app.cell
@@ -105,38 +105,136 @@ def _(gb_bounding_box, geospatial, pl, spatial_index_df):
 
     from polars import testing as pl_testing
 
-    _spatial_index = geospatial.SpatialIndex(gb_bounding_box)
-    assert len(spatial_index_df) == len(_spatial_index)
+    gb_spatial_index = geospatial.SpatialIndex(gb_bounding_box)
+    assert len(spatial_index_df) == len(gb_spatial_index)
     _computed_spatial_index = (
         spatial_index_df.select(["latitude", "longitude"])
-        .map_rows(lambda _row: _spatial_index.lat_lon_to_index(lat=_row[0], lon=_row[1]))
+        .map_rows(lambda _row: gb_spatial_index.lat_lon_to_index(lat=_row[0], lon=_row[1]))
         .to_series()
         .rename("spatial_index")
         .cast(pl.UInt16)
     )
 
     pl_testing.assert_series_equal(_computed_spatial_index, spatial_index_df["spatial_index"])
-    return
+    return (gb_spatial_index,)
 
 
 @app.cell
-def _():
+def _(gb_spatial_index, pl):
     """Get spatial indexes for PV systems."""
+
+    pv_systems = pl.read_csv("~/data/uk_pv/metadata.csv", try_parse_dates=True)
+    pv_systems
+
+    pv_system_spatial_index = (
+        pv_systems.select(["latitude_rounded", "longitude_rounded"])
+        .map_rows(lambda _row: gb_spatial_index.lat_lon_to_index(lat=_row[0], lon=_row[1]))
+        .to_series()
+        .rename("spatial_index")
+        .cast(pl.UInt16)
+        .unique()
+        .sort()
+        .implode()
+    )
+
+    pv_system_spatial_index
+    return pv_system_spatial_index, pv_systems
+
+
+@app.cell
+def _(pv_systems):
+    pv_systems
     return
 
 
 @app.cell
-def _(mo):
-    mo.md(
-        r"""
-    Next steps:
+def _(
+    date,
+    ds,
+    gb_bounding_box,
+    pl,
+    pv_system_spatial_index,
+    spatial_index_df,
+    timedelta,
+):
+    import pathlib
 
-    - Load a day at a time.
-    - Loop round days and NWP variables.
-    - Use multiple threads to load the xarray data.
-    - Store a monthly Parquet when we've loaded a month.
-    """
-    )
+
+    def load_month_of_era5_and_save_parquet(
+        start_date: date, variable_name: str, output_path_root: pathlib.Path, overwrite: bool = False
+    ):
+        # Check if output file already exists:
+        full_output_path = (
+            output_path_root / variable_name / f"year={start_date.year}/month={start_date.month:02d}"
+        )
+        if (full_output_path / "data.parquet").exists() and not overwrite:
+            return
+
+        # Load ERA5 from Zarr on Google Cloud:
+        end_datetime = (start_date + timedelta(days=40)).replace(day=1) - timedelta(hours=1)
+        xr_ds = (
+            ds[variable_name]
+            .sel(
+                latitude=gb_bounding_box.north_south_slice(),
+                longitude=gb_bounding_box.filter_longitudes_360(ds.longitude.values),
+                time=slice(start_date, end_datetime),
+            )
+            .load()
+        )
+
+        # Convert to Polars DataFrame:
+        df = pl.from_pandas(xr_ds.to_dataframe().reset_index())
+        del xr_ds
+        df = (
+            df
+            # Convert longitude from ERA5's [0, 360) range, to [-180, +180]:
+            .with_columns(
+                longitude=pl.when(pl.col.longitude > 180)
+                .then(pl.col.longitude - 360)
+                .otherwise(pl.col.longitude)
+            )
+            .join(spatial_index_df, on=["latitude", "longitude"], how="left")
+            .drop(["latitude", "longitude"])
+            .filter(pl.col.spatial_index.is_in(pv_system_spatial_index))
+            .sort(["spatial_index", "time"])
+        )
+
+        full_output_path.mkdir(exist_ok=True, parents=True)
+        df.write_parquet(full_output_path / "data.parquet", statistics="full")
+
+
+    # load_month_of_era5_and_save_parquet(
+    #    datetime(2025, 3, 1, 0, 0),
+    #    "surface_solar_radiation_downwards",
+    #    pathlib.Path("~/data/ERA5/").expanduser(),
+    # )
+    return load_month_of_era5_and_save_parquet, pathlib
+
+
+@app.cell
+def _(load_month_of_era5_and_save_parquet, pathlib, pl, pv_systems):
+    """Load data month-by-month"""
+
+    from concurrent import futures
+    import functools
+
+    start_date = pv_systems["start_datetime_GMT"].min().date().replace(day=1)
+    end_date = pv_systems["end_datetime_GMT"].max().date()
+
+    months = pl.date_range(start_date, end_date, interval="1mo", eager=True)
+
+    with futures.ThreadPoolExecutor(max_workers=4) as e:
+        results = e.map(
+            functools.partial(
+                load_month_of_era5_and_save_parquet,
+                variable_name="surface_solar_radiation_downwards",
+                output_path_root=pathlib.Path("~/data/ERA5/").expanduser(),
+            ),
+            months,
+        )
+
+        # Surface any errors raised during concurrent execution:
+        list(results)
     return
 
 
