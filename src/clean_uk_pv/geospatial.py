4d0cac3939884541c2a3e5d6516e24b0293db1bc
Jack Kelly <jack@openclimatefix.org>
2025-05-19 16:03:21 +0100

    Tidying comments. And configuring line length for pycodestyle and docstyle

--- src/clean_uk_pv/geospatial.py
+++ src/clean_uk_pv/geospatial.py
@@ -1,24 +1,30 @@
-"""
-We use a simple discrete spatial index that works like this:
+"""We use a simple discrete spatial index.
 
-- Each NWP pixel is given a unique UInt16 identifier, in row-major order.
-- Create a GeoJSON file which provides the geospatial bounding box of each ERA5 pixel, and maps between the integer ID and the bounding box. The lat lon coords in ARCO-ERA5 refer to the centre of each grid box. Given that ERA5 is 0.25 x 0.25 degrees (from Gemini):
+- Each NWP pixel is given a unique UInt16 identifier, in row-major order, starting from the top
+left.
 
-    Consider an ERA5 grid point with a reported latitude of 50.0° N and a longitude of 0.5° E. The bounding box for this cell would be:
+Note that the lat lon coords in ARCO-ERA5 refer to the _centre_ of each grid box.
 
-    Minimum Latitude: 50.0 - 0.125 = 49.875° N
-    Maximum Latitude: 50.0 + 0.125 = 50.125° N
-    Minimum Longitude: 0.5 - 0.125 = 0.375° E
-    Maximum Longitude: 0.5 + 0.125 = 0.625° E
+Given that ERA5 is 0.25 x 0.25 degrees (from Gemini):
 
-- Use [Polars-ST](https://oreilles.github.io/polars-st/api-reference/) to map from the lat lon of each PV system to the spatial index.
-- Plot with a [Choropleth map in Vega-Altair](https://altair-viz.github.io/gallery/choropleth.html).
-    - For example, I could plot a count of the PV systems within each ERA5 grid cell.
-- Finding neighbours is trivial, because we've used a simple row-major numbering system. So we just do some simple integer maths.
+Consider an ERA5 grid point with a reported latitude of 50.0° N and a longitude of 0.5° E. The
+bounding box for this cell would be:
 
+Minimum Latitude: 50.0 - 0.125 = 49.875° N
+Maximum Latitude: 50.0 + 0.125 = 50.125° N
+Minimum Longitude: 0.5 - 0.125 = 0.375° E
+Maximum Longitude: 0.5 + 0.125 = 0.625° E
+
+Finding neighbours is trivial, because we've used a simple row-major numbering system.
+So we just do some simple integer maths.
 
 ## Design paths not taken
-I originally wanted to use H3 as my spatial index but it appears that no H3 resolution is a particularly good fit for ERA5. To use H3, I'd either have to throw information away by using a too-low H3 resolution; or I'd have to [interpolate](https://pysal.org/tobler/notebooks/census_to_hexgrid.html) ERA5 to create a dense higher resolution H3 grid. Neither solution is very attractive.
+We originally wanted to use H3 as our spatial index but it appears that no
+H3 resolution is a particularly good fit for ERA5. To use H3, we'd either have to throw information
+away by using a too-low H3 resolution; or we'd have to
+[interpolate](https://pysal.org/tobler/notebooks/census_to_hexgrid.html)
+ERA5 to create a dense higher resolution H3 grid. Neither solution is very attractive.
+
 """
 
 from collections.abc import Iterable
@@ -26,16 +32,16 @@ from collections.abc import Iterable
 
 class BoundingBox:
     def __init__(self, north: float, south: float, east: float, west: float):
-        """
-        Initialize the bounding box.
+        """Initialize the bounding box.
 
         The bounding box is defined by the range [south, north), [west, east).
 
-        Please provide longitudes in the range [-180, 180] degrees, where 0 represents the Prime Meridian
-        (passing through Greenwich), positive longitudes are east of Greenwich, and
+        Please provide longitudes in the range [-180, 180] degrees, where 0 represents the Prime
+        Meridian (passing through Greenwich), positive longitudes are east of Greenwich, and
         negative longitudes are west of Greenwich.
 
-        We do not yet support bounding boxes which cross the anti-Meridian (the line of longitude at 180 degrees).
+        We do not yet support bounding boxes which cross the anti-Meridian (the line of longitude at
+        180 degrees).
 
         Parameters
         ----------
@@ -43,6 +49,7 @@ class BoundingBox:
         south: The southernmost latitude of the bounding box.
         east: The easternmost longitude of the bounding box.
         west: The westernmost longitude of the bounding box.
+
         """
         # Lots of sanity checks:
         for name, value in (("west", west), ("east", east)):
@@ -79,18 +86,14 @@ class BoundingBox:
         self.height_degrees = north - south
 
     def north_south_east_west(self) -> tuple[float, float, float, float]:
-        """
-        Return the bounding box as a tuple of (north, south, east, west).
-        """
+        """Return the bounding box as a tuple of (north, south, east, west)."""
         return self.north, self.south, self.east, self.west
 
     def north_south_slice(self) -> slice:
         return slice(self.north, self.south)
 
     def filter_longitudes_360(self, longitudes: Iterable[float]) -> Iterable[float]:
-        """
-        Filter a list of longitudes encoded as [0, 360) degrees east of Greenwich.
-        """
+        """Filter a list of longitudes encoded as [0, 360) degrees east of Greenwich."""
         west_360 = self.west + 360 if self.west < 0 else self.west
         east_360 = self.east + 360 if self.east < 0 else self.east
         if not ((0 <= max(longitudes) < 360) and (0 <= min(longitudes) < 360)):
@@ -112,34 +115,29 @@ class BoundingBox:
 
 
 class GBBoundingBox(BoundingBox):
-    """
-    A bounding box for Great Britain.
-    """
+    """A bounding box for Great Britain."""
 
     def __init__(self):
-        """
-        Initialize the bounding box for Great Britain.
+        """Initialize the bounding box for Great Britain.
 
-        Note that the coordinates in ERA5 specify the *central* point of each grid cell,
-        yet our BoundingBox defines the *edges* of the bounding box. Hence when the edges are offset
-        by 0.125 degrees.
+        Note that the coordinates in ERA5 specify the *central* point of each grid cell, yet our
+        BoundingBox defines the *edges* of the bounding box. Hence when the edges are offset by
+        0.125 degrees.
+
         """
         super().__init__(north=59.625, south=49.625, east=2.125, west=-8.125)
 
 
 class SpatialIndex:
-    """
-    A discrete spatial index, in row-major order, starting from the top left
-    (north west) corner.
-    """
+    """A discrete spatial index, in row-major order, starting from the top left (north west)
+    corner."""
 
     def __init__(
         self,
         bounding_box: BoundingBox,
         resolution_degrees: float = 0.25,
     ):
-        """
-        Initialize the SpatialIndex.
+        """Initialize the SpatialIndex.
 
         Parameters
         ----------
@@ -148,6 +146,7 @@ class SpatialIndex:
             For example, for ERA5, the resolution is 0.25 degrees.
             The resolution must exactly divide `bounding_box.width_degrees` and
             `bounding_box.height_degrees`.
+
         """
         self.bounding_box = bounding_box
         self.resolution_degrees = resolution_degrees
@@ -164,10 +163,9 @@ class SpatialIndex:
         self.n_cols = int(bounding_box.width_degrees / resolution_degrees)
 
     def lat_lon_to_index(self, lat: float, lon: float) -> int:
-        """
-        Convert latitude and longitude to a grid index. Note that this function floors the
-        lat/lon to the nearest north-westerly grid box, which is what we need for associating lat/lons with
-        NWP grid boxes.
+        """Convert latitude and longitude to a grid index. Note that this function floors the
+        lat/lon to the nearest north-westerly grid box, which is what we need for associating
+        lat/lons with NWP grid boxes.
 
         Parameters
         ----------
@@ -176,7 +174,8 @@ class SpatialIndex:
 
         Returns
         -------
-        The grid index of the point.
+        index: The spatial index.
+
         """
         north, south, east, west = self.bounding_box.north_south_east_west()
         if not (south <= lat < north):
@@ -191,7 +190,5 @@ class SpatialIndex:
         return row * self.n_cols + col
 
     def __len__(self) -> int:
-        """
-        Return the number of grid cells in the spatial index.
-        """
+        """Return the number of grid cells in the spatial index."""
         return self.n_rows * self.n_cols
